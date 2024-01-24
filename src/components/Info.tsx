32e689bb8d3661122bc6c89c8b497044ce0a8d9a
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2024-01-24 21:29:07 +0100

    Update Info.tsx

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -5,16 +5,15 @@ interface InfoProp {
 export function Info(p: InfoProp) {
   return (
     <div className="absolute w-full h-full top-0 left-0 bg-sky-300/95 py-8 z-50" onClick={p.toggleInfo}>
-      <h1 className="text-4xl text-center font-bold mb-4">What is this?</h1>
+      <h1 className="text-4xl text-center font-bold mb-4">Cos'è questo sito?</h1>
       <div className="text-sm px-2">
         <div className="absolute top-1 right-1 text-4xl cursor-pointer">✖️</div>
-        <p className="pb-2">This website is not affiliated with KinderTap, just made by a parent driven (mad?) by curiosity and struggling with insomnia.</p>
-        <p className="pb-2">I don't collect any info, won't save your credentials, you can choose to keep yourself authenticated (for up to 3 months, but it depends on KinderTap) and in that case the website will save ONLY ON YOUR BROWSER an authentication token.</p>
-        <p className="pb-4">This is completely free, and always will be. You can contact me via email using the mail symbol on the homepage. Be kind.</p>
-        <p className="pb-2 font-bold">WARNING: somewhat technical stuff ahead.</p>
-        <p className="pb-2">The website is hosted on Netlify, and the code is open source. The two functions hosted on Netlify do not log or send your data anywhere else.</p>
-        <p className="pb-2">You'll see a couple of requests made to <code>/.netlify/functions...</code>: I only use those to go around CORS rules enforced by KinderTap, since there is no other way of getting authenticated.</p>
-        <p className="pb-2">Feel free to take a look at the code using the GitHub link on the homepage, or click <a className="text-red-700" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">here</a>. Any issue or PR is welcomed!</p>
+        <p className="pb-1">1 - né io né questo sito siamo collegati direttamente a KinderTip, ne usiamo solo i dati.</p>
+        <p className="pb-1">2 - non ricevo né mi salvo nessun vostro dato, potete scegliere di salvare l'accesso (per circa 3 mesi), e in tal caso SOLO SUL VOSTRO BROWSER verrà salvata l'autenticazione.</p>
+        <p className="pb-2">3 - questo sito è e resterà ovviamente gratuito e open source, in homepage trovate codice e un'email dove contattarmi se volete.</p>
+        <p className="pb-1 font-bold">ATTENZIONE: roba un po' tecnica ora.</p>
+        <p className="pb-1">Il sito è hostato da Netlify, e usa due Netlify functions (tipo lambda di aws diciamo) per aggirare le regole CORS implementate dal servet di KinderTap. Le funzioni non si salvano nulla, né loggano, ma potete controllare dal codice appunto.</p>
+        <p className="pb-1">Sempre sul repository, linkato in homepage o <a className="text-red-700" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">qui</a> potete aprire issues, ed eventuali PR sono benvenute!</p>
       </div>
     </div>
   )
