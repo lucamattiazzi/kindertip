b34baf2e3acd14eca093239578359628c0c279d3
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2024-01-24 15:54:56 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ interface HeaderProps {
 export function Header(p: HeaderProps) {
   return (
     <div>
-      <h1 className="text-4xl text-center font-bold mb-1">KinderTip</h1>
+      <h1 className="text-3xl text-center font-bold mb-1">KinderTip</h1>
       <p className="px-6 text-xxs text-center">or: how to use your kid's KinderTap diary to see cool numbers and plots.</p>
       <div className="flex justify-around items-center mb-4">
         <a href="https://github.com/lucamattiazzi/kindertip" target="_blank">
-          <img width="30" height="30" src="/github.png" />
+          <img width="25" height="30" src="/github.png" />
         </a>
         <div className="emoji-button" onClick={p.toggleInfo}>ℹ️</div>
         <a className="emoji-button" href="mailto:l.d.mattiazzi@gmail.com?subject=Something about KinderTip" target="_blank">📧</a>
